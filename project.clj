354f0c0b15e96681ad6105b1ac54cee9a040e15c
Aaron <apeckham@gmail.com>
2022-11-26 21:32:44 +0000

    fix version number

--- project.clj
+++ project.clj
@@ -1,4 +1,12 @@
-(defproject com.urbandictionary/malli-env "0.1.0-SNAPSHOT"
+(defproject com.urbandictionary/malli-env "0.1.1"
+  :description "Get environment variables with malli validation and coercion"
+  :url "https://github.com/urbandictionary/malli-env"
   :profiles {:dev {:dependencies [[metosin/malli "0.8.4"]]}
              :provided {:dependencies [[metosin/malli "0.8.4"]]}}
+  :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["clojars"
+                         {:password :env/clojars_password
+                          :sign-releases false
+                          :url "https://repo.clojars.org"
+                          :username :env/clojars_username}]]
   :dependencies [[org.clojure/clojure "1.10.0"]])
